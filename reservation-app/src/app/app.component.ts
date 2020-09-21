e19f0948f4fa195af033b8ac0e787c4e25b17099
Oscar Gueita <oscar.gueita@lastminute.com>
2020-09-21 21:32:58 +0200

    More changes

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reservation-app';
+
+  constructor(private http:HttpClient) {}
+  private baseUrl:string = 'http://localhost:8080';
+  private reservationurl:string = this.baseUrl + '/room/v1/reservation/';
 }
